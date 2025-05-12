8984c5df88a9760aceff6fae04a7949b9064164b
yanns <yannn.savin2006@gmail.com>
2025-05-12 21:27:58 +0200

    yann

---
+++ cmake-build-debug/CMakeFiles/ECEFUS.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/yanns/CLionProjects/ECEFUS/affichage.c" "CMakeFiles/ECEFUS.dir/affichage.c.obj" "gcc" "CMakeFiles/ECEFUS.dir/affichage.c.obj.d"
+  "C:/Users/yanns/CLionProjects/ECEFUS/init.c" "CMakeFiles/ECEFUS.dir/init.c.obj" "gcc" "CMakeFiles/ECEFUS.dir/init.c.obj.d"
+  "C:/Users/yanns/CLionProjects/ECEFUS/main.c" "CMakeFiles/ECEFUS.dir/main.c.obj" "gcc" "CMakeFiles/ECEFUS.dir/main.c.obj.d"
+  "C:/Users/yanns/CLionProjects/ECEFUS/plateau.c" "CMakeFiles/ECEFUS.dir/plateau.c.obj" "gcc" "CMakeFiles/ECEFUS.dir/plateau.c.obj.d"
+  "C:/Users/yanns/CLionProjects/ECEFUS/spell.c" "CMakeFiles/ECEFUS.dir/spell.c.obj" "gcc" "CMakeFiles/ECEFUS.dir/spell.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ECEFUS.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ECEFUS.dir/affichage.c.obj"
+  "CMakeFiles/ECEFUS.dir/affichage.c.obj.d"
+  "CMakeFiles/ECEFUS.dir/init.c.obj"
+  "CMakeFiles/ECEFUS.dir/init.c.obj.d"
+  "CMakeFiles/ECEFUS.dir/main.c.obj"
+  "CMakeFiles/ECEFUS.dir/main.c.obj.d"
+  "CMakeFiles/ECEFUS.dir/plateau.c.obj"
+  "CMakeFiles/ECEFUS.dir/plateau.c.obj.d"
+  "CMakeFiles/ECEFUS.dir/spell.c.obj"
+  "CMakeFiles/ECEFUS.dir/spell.c.obj.d"
+  "ECEFUS.exe"
+  "ECEFUS.exe.manifest"
+  "ECEFUS.pdb"
+  "libECEFUS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ECEFUS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
